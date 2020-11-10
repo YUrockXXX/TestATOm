0f5b0f1b5c6d9330d03de9cf27f7da43ef62870a
YUrock <yuragol2000@gmail.com>
2020-11-10 14:32:18 +0300

    test commit

--- Week_2/UpdateIfGreater.cpp
+++ Week_2/UpdateIfGreater.cpp
@@ -5,14 +5,3 @@ void UpdateIfGreater(const int first, int& second){
     second = first;
   }
 }
-// int main(){
-//   int x = 4;
-//   int y = 1;
-//
-//   std::cout << y << '\n';
-//
-//   UpdateIfGreater(5,y);
-//
-//   std::cout << y << '\n';
-//   return 0;
-// }
